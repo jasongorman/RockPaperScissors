16c3961b02ead0bc2e5c74e6457165eb5c2a5e59
Jason Gorman <jason.gorman@codemanship.com>
2018-03-14 07:45:22 +0000

    Fixed static constant object creation bug that made first test fail

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,5 @@
 
-public class Player {
+class Player {
 
 	private Player beats;
 
--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,9 +1,9 @@
 
 public class RockPaperScissors {
 
-	static Player SCISSORS;
-	static Player PAPER;
-	static Player ROCK;
+	static Player SCISSORS = new Player();
+	static Player PAPER = new Player();
+	static Player ROCK = new Player();
 	
 	static final int PLAYER_TWO_WINS = 2;
 	static final int PLAYER_ONE_WINS = 1;
@@ -11,9 +11,6 @@ public class RockPaperScissors {
 	static final int INVALID_PLAY = -1;
 	
 	public RockPaperScissors(){
-		SCISSORS = new Player();
-		PAPER = new Player();
-		ROCK = new Player();
 		SCISSORS.setBeats(PAPER);
 		PAPER.setBeats(ROCK);
 		ROCK.setBeats(SCISSORS);
